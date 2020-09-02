90d6a6e7ae18ce07b0c44338b2bd64d77f0c53c4
Aidan Torrance <aidantorrance@Aidans-MacBook-Pro.local>
2020-09-02 16:44:24 +1200

    Made login

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Express' });
+});
+
+
+
 
 module.exports = router;
